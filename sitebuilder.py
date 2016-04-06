269551c0df9df7934d26f4cce6cecf4b38debc34
Ravi Kotecha <kotecha.ravi@gmail.com>
2016-04-06 17:18:58 +0100

    remove photos

--- sitebuilder.py
+++ sitebuilder.py
@@ -8,11 +8,9 @@ import datetime
 from commands import handle_command
 from flask import Flask, render_template, send_from_directory, request, json, g
 from flask_flatpages import FlatPages
-import flickr
 import markdown2
 from flask_frozen import Freezer
 from jinja2.exceptions import TemplateNotFound
-# from flaskext.compass import Compass
 import flask_assets
 from settings import FLICKR_API_KEY, FLICKR_API_SECRET
 from webassets import Bundle
@@ -61,12 +59,8 @@ freezer = Freezer(app)
 #compass = Compass(app)
 
 common_js = Bundle('js/modernizr.custom.59903.js', 'js/jquery-1.9.1.min.js', output='js/gen/common.js', filters='rjsmin')
-photo_js = Bundle('coffeescript/imagebg.coffee', output='js/gen/imagebg.js', filters='coffeescript,rjsmin')
-equalheights_js = Bundle('js/jquery.equalheights.min.js', output='js/gen/equalheights.js', filters='rjsmin')
 
-assets.register('photo_js', photo_js)
 assets.register('common_js', common_js)
-assets.register('equalheights_js', equalheights_js)
 
 
 
@@ -74,7 +68,6 @@ live_pages = [x for x in pages if not x.meta.get('draft', False)]
 MODELS = {
     'shorts': sorted([p for p in live_pages if p.path.startswith('short/')], key=lambda x: x.meta.get('date'), reverse=True),
     'posts': sorted([p for p in live_pages if p.path.startswith('post/')], key=lambda x: x.meta.get('date'), reverse=True),
-    'photos': sorted([p for p in live_pages if p.path.startswith('photo/')], key=lambda x: x.meta.get('date'), reverse=True),
     'milestones': sorted([p for p in live_pages if p.path.startswith('milestone/')], key=lambda x: x.meta.get('date'), reverse=True),
 }
 MODELS['all'] = sorted(itertools.chain.from_iterable(MODELS.values()), key=lambda x: x.meta.get('date'), reverse=True)
@@ -88,7 +81,6 @@ def before_req():
         MODELS = {
             'shorts': sorted([p for p in live_pages if p.path.startswith('short/')], key=lambda x: x.meta.get('date'), reverse=True),
             'posts': sorted([p for p in live_pages if p.path.startswith('post/') and (not getattr(p, 'draft', False))], key=lambda x: x.meta.get('date'), reverse=True),
-            'photos': sorted([p for p in live_pages if p.path.startswith('photo/')], key=lambda x: x.meta.get('date'), reverse=True),
             'milestones': sorted([p for p in live_pages if p.path.startswith('milestone/')], key=lambda x: x.meta.get('date'), reverse=True),
             }
         MODELS['all'] = sorted(itertools.chain.from_iterable(MODELS.values()), key=lambda x: x.meta.get('date'), reverse=True)
@@ -131,12 +123,6 @@ def post_index():
     posts = [p for p in MODELS.get('posts') if 'draft' not in p.meta.get('tags', [])]
     return render_template('post_index.jade', posts=posts, title='Posts')
 
-@app.route('/photo/')
-def photo_index():
-    photos = MODELS.get('photos')
-    return render_template('photo_index.jade', photos=photos, title='Photos')
-
-
 @app.route('/cv/')
 def cv_index():
     return render_template('cv.jade', title='Curriculum Vitae')
@@ -150,25 +136,6 @@ def _to_default_dict(d):
     ret = defaultdict(lambda: orig)
     ret.update(d)
     return ret
-
-@app.route('/photo/<path:path>/')
-def photo_entry(path):
-    full_path = 'photo/' + path
-    page = pages.get_or_404(full_path)
-    all_photos = MODELS.get('photos')
-    idx = all_photos.index(page)
-    prev_page = all_photos[idx-1]
-    next_page = all_photos[(idx+1) % len(all_photos)]
-
-    sizes = _to_default_dict(page.meta.get('sizes')) or flickr.sizes_from_id(page.meta.get('photo_id'))
-
-    return render_template('photo.jade', page=page,
-        sizes=sizes,
-        title=page.meta.get('title', 'Untitled'),
-        bgcolor=page.meta.get('bgcolor'),
-        prev_page = '/'+ prev_page.path,
-        next_page = '/' + next_page.path
-    )
 
 @app.route('/<path:path>/')
 def page(path):
